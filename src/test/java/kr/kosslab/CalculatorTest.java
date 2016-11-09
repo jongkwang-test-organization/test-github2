7d7ddf83e795ee392e3ee5061d8cf890be71e2d3
JongKwang Kim <kim@jongkwang.com>
2016-11-10 02:05:42 +0900

    Init Calculator class with Gradle

---
+++ src/test/java/kr/kosslab/CalculatorTest.java
@@ -0,0 +1,17 @@
+package kr.kosslab;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by JongKwang on 10/11/2016.
+ */
+public class CalculatorTest {
+    @Test
+    public void sum() throws Exception {
+        Calculator c = new Calculator();
+        assertEquals(c.sum(1, 2), 3);
+    }
+
+}
